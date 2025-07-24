962cb68ba5d1d257bba8a3fe7c99d344c5c9fecb
rohitsingh <rohitsingh986491@gmail.com>
2025-07-25 01:23:56 +0530

    updaated api base url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
-// const API_BASE_URL = "https://quizeapp-backend-3ma3.onrender.com";
+// const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://quizeapp-backend-3ma3.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
